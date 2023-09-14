78a4b17e9c2647c7c313ad5e1fc35a832fea0b11
Khairi Rahimi <Khairi.rahimi21122@gmail.com>
2023-09-14 10:24:59 +0800

    first commit

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('header')
+<h1>Product</h1>
+@endsection
+
+@section('content')
+<div class="d-flex justify-content-end" >
+    <a class="btn btn-success" href="{{url(route('product.create')) }}">
+        <i class="bi bi-file-earmark-plus-fill"></i> Add New Product</a>
+
+</div>
+
+    <table class="table mt-3">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+            @php
+                $index =1
+            @endphp
+            @forelse ($products as $product)
+        <tr>
+            <th scope="row">1</th>
+            <td>{{$product->name}}</td>
+            <td>{{$product->desc}}</td>
+            <td>{{$product->price}}</td>
+            <td>{{$product->stock}}</td>
+            <td></td>
+        </tr>
+        @empty
+        <tr>
+            <th colspan="6" scope="row"class="text-center"> wow so empty..</th>
+        </tr>
+
+            @endforelse      
+        </tbody>
+    </table>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+// Route::get('/product.index', function () {
+//     return "<p>This is product pages</p>";
+// })->name('product');
+
+Route::get('/product',[ProductController::class,'index'])->name('product.index');
+Route::get('/product/create',[ProductController::class,'create'])->name('product.create');
+Route::post('/product/store',[ProductController::class,'store'])->name('product.store');
+
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
